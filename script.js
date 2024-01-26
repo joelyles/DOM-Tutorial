fae157384223e467bdb3adee6b06c8463fcfc3f2
Joe Lyles <lylesjoe@protonmail.com.com>
2024-01-26 05:58:50 -0500

    setup/styling for DOM tutorial and initial notes

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+// DOM Manipulation
 
+/* primary selectors will be querySeletor and querySelectorAll due to versatility in selecting ids, classes, tags, etc. */
+
+
+
+
+// getElementById()
+
+/* const title = document.getElementById('main-heading');
+console.log(title); */
+
+
+
+
+// getElementByClassName()
+
+/* const listItem = document.getElementsByClassName('list-item');
+console.log(listItem); */
+
+
+
+
+// getElementByTagName()
+
+/* const listItem = document.getElementsByTagName('li');
+console.log(listItem); */
+
+
+
+
+// querySelector() match first option of the options
+
+/* const container = document.querySelector('div');
+console.log(container); */
+
+
+
+
+// querySelectorAll()
+
+/* const container = document.querySelectorAll('div');
+console.log(container); */
